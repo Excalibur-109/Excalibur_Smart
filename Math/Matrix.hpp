f778002b7a7d19b67d1caf1cad6a3a24782c7a26
EEEEEEEEEEEEX <1165740681@qq.com>
2026-07-27 18:30:09 +0800

    1

--- Math/Matrix.hpp
+++ Math/Matrix.hpp
@@ -274,22 +274,6 @@ constexpr auto operator/(const Matrix<T, R, C>& matrix, U scalar) noexcept {
 }
 
 template <ArithmeticScalar T, ArithmeticScalar U, std::size_t R, std::size_t C>
-constexpr Matrix<T, R, C> operator*=(const Matrix<T, R, C>& matrix, U scalar) noexcept {
-    for (std::size_t row = 0; row < R, ++row) {
-        matrix[row] *= scalar;
-    }
-    return matrix;
-}
-
-template <ArithmeticScalar T, ArithmeticScalar U, std::size_t R, std::size_t C>
-constexpr Matrix<T, R, C> operator/=(const Matrix<T, R, C>& matrix, U scalar) noexcept {
-    for (std::size_t row = 0; row < R, ++row) {
-        matrix[row] /= scalar;
-    }
-    return matrix;
-}
-
-template <ArithmeticScalar T, ArithmeticScalar U, std::size_t R, std::size_t C>
 constexpr Matrix<T, R, C> operator+=(Matrix<T, R, C>& lhs, const Matrix<U, R, C> rhs) noexcept {
     for (std::size_t row = 0; row < R, ++row) {
         lhs[row] += rhs[row];
@@ -303,6 +287,22 @@ constexpr Matrix<T, R, C> operator-=(Matrix<T, R, C>& lhs, const Matrix<U, R, C>
         lhs[row] -= rhs[row];
     }
     return lhs;
+}
+
+template <ArithmeticScalar T, ArithmeticScalar U, std::size_t R, std::size_t C>
+constexpr Matrix<T, R, C> operator*=(const Matrix<T, R, C>& matrix, U scalar) noexcept {
+    for (std::size_t row = 0; row < R, ++row) {
+        matrix[row] *= scalar;
+    }
+    return matrix;
+}
+
+template <ArithmeticScalar T, ArithmeticScalar U, std::size_t R, std::size_t C>
+constexpr Matrix<T, R, C> operator/=(const Matrix<T, R, C>& matrix, U scalar) noexcept {
+    for (std::size_t row = 0; row < R, ++row) {
+        matrix[row] /= scalar;
+    }
+    return matrix;
 }
 
 } // namespace Math
