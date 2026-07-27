b41fb7d321c483a3a1a94b112704c6e981dad8e5
EEEEEEEEEEEEX <1165740681@qq.com>
2026-07-27 17:50:16 +0800

    1

--- Math/Matrix.hpp
+++ Math/Matrix.hpp
@@ -26,6 +26,8 @@ namespace Math
         static constexpr std::size_t Column = C;
         std::array<Vector<T, C>, R> rows{};
 
+        constexpr Matrix() noexcept = default;
+
         explicit constexpr Matrix(T digonal) noexcept requires(R == C) {
             for (std::size_t index = 0; index < R; ++index) {
                 rows[index][index] = digonal;
@@ -122,6 +124,89 @@ EXCALIBUR_FORCE_INLINE float DotFloat3(__m128 lhs, __m128 rhs) noexcept {
     return _mm_cvtss_f32(sum);
 }
 
+EXCALIBUR_FORCE_INLINE bool TryInverseFloat4x4(const Matrix<float, 4, 4>& matrix, Matrix<float, 4, 4>* output, float epsilon) noexcept {
+    __m128 a = LoadFloat4(matrix.rows[0]);
+    __m128 b = LoadFloat4(matrix.rows[1]);
+    __m128 c = LoadFloat4(matrix.rows[2]);
+    __m128 d = LoadFloat4(matrix.rows[3]);
+    __m128 aw = _mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 3, 3, 3));
+    __m128 bw = _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 3, 3, 3));
+    __m128 cw = _mm_shuffle_ps(c, c, _MM_SHUFFLE(3, 3, 3, 3));
+    __m128 dw = _mm_shuffle_ps(d, d, _MM_SHUFFLE(3, 3, 3, 3));
+
+    __m128 s = CrossFloat3(a, b);
+    __m128 t = CrossFloat3(c, d);
+    __m128 u = _mm_sub_ps(_mm_mul_ps(a, bw), _mm_mul_ps(b, aw));
+    __m128 v = _mm_sub_ps(_mm_mul_ps(c, dw), _mm_mul_ps(d, cw));
+    const float determinant = DotFloat3(s, v) + DotFloat3(t, u);
+    if (Abs(determinant) <= epsilon) {
+        return false;
+    }
+
+    __m128 inverseDeterminant = _mm_set1_ps(1.0F / determinant);
+    s = _mm_mul_ps(s, inverseDeterminant);
+    t = _mm_mul_ps(t, inverseDeterminant);
+    u = _mm_mul_ps(u, inverseDeterminant);
+    v = _mm_mul_ps(v, inverseDeterminant);
+    const __m128 row0 = _mm_add_ps(CrossFloat3(b, v), _mm_mul_ps(t, bw));
+    const __m128 row1 = _mm_sub_ps(CrossFloat3(v, a), _mm_mul_ps(t, aw));
+    const __m128 row2 = _mm_add_ps(CrossFloat3(d, u), _mm_mul_ps(s, dw));
+    const __m128 row3 = _mm_sub_ps(CrossFloat3(u, c), _mm_mul_ps(s, cw));
+    StoreFloat4(output->rows[0], row0);
+    StoreFloat4(output->rows[1], row1);
+    StoreFloat4(output->rows[2], row2);
+    StoreFloat4(output->rows[3], row3);
+    output->rows[0].w = -DotFloat3(b, t);
+    output->rows[1].w = DotFloat3(a, t);
+    output->rows[2].w = -DotFloat3(d, s);
+    output->rows[3].w = DotFloat3(c, s);
+    __m128 outputRow0 = LoadFloat4(output->rows[0]);
+    __m128 outputRow1 = LoadFloat4(output->rows[1]);
+    __m128 outputRow2 = LoadFloat4(output->rows[2]);
+    __m128 outputRow3 = LoadFloat4(output->rows[3]);
+    _MM_TRANSPOSE4_PS(outputRow0, outputRow1, outputRow2, outputRow3);
+    StoreFloat4(output->rows[0], outputRow0);
+    StoreFloat4(output->rows[1], outputRow1);
+    StoreFloat4(output->rows[2], outputRow2);
+    StoreFloat4(output->rows[3], outputRow3);
+    return true;
+}
+
+EXCALIBUR_FORCE_INLINE Vector<float, 4> MultiplyFloat4x4Vector(const Matrix<float, 4, 4>& matrix, const Vector<float, 4>& vector) noexcept {
+    __m128 column0 = LoadFloat4(matrix.rows[0]);
+    __m128 column1 = LoadFloat4(matrix.rows[1]);
+    __m128 column2 = LoadFloat4(matrix.rows[2]);
+    __m128 column3 = LoadFloat4(matrix.rows[3]);
+    _MM_TRANSPOSE4_PS(column0, column1, column2, column3);
+    __m128 input = LoadFloat4(vector);
+    __m128 xxxx = _mm_shuffle_ps(input, input, _MM_SHUFFLE(0, 0, 0, 0));
+    __m128 yyyy = _mm_shuffle_ps(input, input, _MM_SHUFFLE(1, 1, 1, 1));
+    __m128 zzzz = _mm_shuffle_ps(input, input, _MM_SHUFFLE(2, 2, 2, 2));
+    __m128 wwww = _mm_shuffle_ps(input, input, _MM_SHUFFLE(3, 3, 3, 3));
+    __m128 result = _mm_mul_ps(column0, xxxx);
+    result = _mm_add_ps(result, _mm_mul_ps(column1, yyyy));
+    result = _mm_add_ps(result, _mm_mul_ps(column2, zzzz));
+    result = _mm_add_ps(result, _mm_mul_ps(column3, wwww));
+    Vector<float, 4> output{};
+    StoreFloat4(output, result);
+    return output;
+}
+
+EXCALIBUR_FORCE_INLINE Matrix<float, 4, 4> MultiplyFloat4x4(const Matrix<float, 4, 4>& lhs, const Matrix<float, 4, 4>& rhs) noexcept {
+    __m128 rhsRow0 = LoadFloat4(rhs.rows[0]);
+    __m128 rhsRow1 = LoadFloat4(rhs.rows[1]);
+    __m128 rhsRow2 = LoadFloat4(rhs.rows[2]);
+    __m128 rhsRow3 = LoadFloat4(rhs.rows[3]);
+    Matrix<float, 4, 4> output{};
+    StoreFloat4(output.rows[0], MultiplyMatrixRow(LoadFloat4(lhs.rows[0]), rhsRow0, rhsRow1, rhsRow2, rhsRow3));    
+    StoreFloat4(output.rows[1], MultiplyMatrixRow(LoadFloat4(lhs.rows[1]), rhsRow0, rhsRow1, rhsRow2, rhsRow3));
+    StoreFloat4(output.rows[2], MultiplyMatrixRow(LoadFloat4(lhs.rows[2]), rhsRow0, rhsRow1, rhsRow2, rhsRow3));
+    StoreFloat4(output.rows[3], MultiplyMatrixRow(LoadFloat4(lhs.rows[3]), rhsRow0, rhsRow1, rhsRow2, rhsRow3));
+    return output;
+}
+
 } // namespace detail
 #endif
+
+
 } // namespace Math
